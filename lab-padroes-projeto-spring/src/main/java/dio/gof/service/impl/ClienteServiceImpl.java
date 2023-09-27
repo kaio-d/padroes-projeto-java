5ba4f013a3db32d89a00511746e35f531f943762
Kaio Dantas Mancini <dantaskaio01@gmail.com>
2023-09-27 12:01:02 -0300

    initial commit

---
+++ lab-padroes-projeto-spring/src/main/java/dio/gof/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,59 @@
+package dio.gof.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dio.gof.model.Cliente;
+import dio.gof.model.ClienteRepository;
+import dio.gof.model.Endereco;
+import dio.gof.model.EnderecoRepository;
+import dio.gof.service.ViaCepService;
+
+
+@Service
+public class ClienteServiceImpl {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
+    public Iterable<Cliente> buscarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente buscarPorId(Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
+    }
+
+    public void inserir(Cliente cliente) {
+        salvarClienteComCep(cliente);
+    }
+
+    public void atualizar(Long id, Cliente cliente) {
+        Optional<Cliente> clienteBd = clienteRepository.findById(id);
+        if (clienteBd.isPresent()) {
+            salvarClienteComCep(cliente);
+        }
+    }
+
+    public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    private void salvarClienteComCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
+    }
+}
---
+++ lab_padroes_projetos_java/src/dio/gof/Teste.java
@@ -0,0 +1,54 @@
+package dio.gof;
+
+import dio.gof.facade.Facade;
+import dio.gof.singleton.SingletonEager;
+import dio.gof.singleton.SingletonLazy;
+import dio.gof.singleton.SingletonLazyHolder;
+import dio.gof.strategy.Comportamento;
+import dio.gof.strategy.ComportamentoAgressivo;
+import dio.gof.strategy.ComportamentoDefensivo;
+import dio.gof.strategy.ComportamentoNormal;
+import dio.gof.strategy.Robo;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		// Testes relacionados ao Design Patterns Singleton
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+		System.out.println(holder);
+		holder = SingletonLazyHolder.getInstancia();
+		System.out.println(holder);
+		
+		// Strategy 
+		
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		
+		// Facade
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("Kaio", "1478524447");
+
+	}
+
+}
